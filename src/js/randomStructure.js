aa0389aeb20f2c88f736e45ae978184b8e4ec1c5
duanmq1994 <duanmq1994@gmail.com>
2023-12-27 14:59:33 +0800

    feat: modify structure

--- src/js/randomStructure.js
+++
@@ -1,9 +0,0 @@
-import structure from '../json/structure.json'
-import * as utils from './utils'
-
-export default function getLinePattern () {
-    let index = utils.getRandomIndexArrayND(structure.length, 10)
-    let index2 = utils.getRandomIndexArray(structure.length, 10)
-    // return structure[index]
-    return index2
-}
