36e8c2e871d9e440ff267d702f90600089402664
duanmq1994 <duanmq1994@gmail.com>
2024-01-01 15:31:29 +0800

    uodate style

--- src/components/Poem.vue
+++ src/components/Poem.vue
@@ -14,14 +14,15 @@ onMounted(() => {
 })
 
 const handleGenerate = () => {
-  poem.value = generate(count, false)
+  poem.value = generate(count.value, false)
 }
 </script>
 
 <template>
   <div class="container">
     <div class="control-container">
-      <input class="slider" type="range" v-bind="count">
+      <span class="control-text">想看{{ count }}句：</span>
+      <input class="slider" type="range" v-model="count" step="1" min="1" max="15">
       <button class="main-button" @click="handleGenerate">生成</button>
     </div>
     <div class="poem-container">
@@ -35,39 +36,56 @@ const handleGenerate = () => {
   padding: 25px;
   display: flex;
   flex-direction: column;
-  gap: 30px;
+  gap: 55px;
   align-items: center;
 }
 .control-container {
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 25px;
   align-items: center;
   width: 100%;
 }
 .slider {
   width: 75%;
+  -webkit-appearance: none;
+  background-image: var(--primary-color);
+  border-radius: 10px;
+  outline: none;
+}
+.slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: #fff;
+    cursor: pointer;
 }
 .main-button {
   border: none;
-  background-color: cornflowerblue;
+  background-image: var(--primary-color);
   color: white;
-  border-radius: 15px;
-  width: 40%;
-  height: 30px;
+  border-radius: 20px;
+  width: 45%;
+  height: 40px;
 }
 .poem-container {
   padding: 15px 25px;
-  border: 3px cornflowerblue solid;
-  border-radius: 10px;
+  border: 8px solid;
+  border-image: var(--primary-color) 20 stretch;
   width: 60%;
   max-width: 500px;
   height: max-content;
-  min-height: 80px;
+  min-height: 50px;
   text-align: center;
+}
+.control-text {
+  font-size: 16px;
+  color: var(--text-color);
 }
 .poem-text {
   font-size: 15px;
-  color: #272727;
+  color: var(--text-color);
+  line-height: 30px;
 }
 </style>
